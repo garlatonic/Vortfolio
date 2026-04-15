71af0e0f20091be03f91e2488eba77e3318328a4
ivoryShield <sangapark34@naver.com>
2026-04-15 16:16:12 +0900

    feat: GitHub 아이콘을 SVG로 변경

--- src/app/(home)/_components/project-item.tsx
+++ src/app/(home)/_components/project-item.tsx
@@ -2,7 +2,6 @@
 
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import GithubIcon from "@/../public/ico/ico_github.svg";
 import Image from "next/image";
 import Link from "next/link";
 import { ExternalLinkIcon, FileTextIcon } from "lucide-react";
@@ -57,7 +56,16 @@ export default function ProjectItem({ project }: { project: Project }) {
           {project.githubLink && (
             <Button size="icon-lg" className="cursor-pointer">
               <Link href={project.githubLink} target="_blank">
-                <GithubIcon />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-github"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                </svg>
                 <span className="sr-only">GitHub에서 프로젝트 보기</span>
               </Link>
             </Button>
