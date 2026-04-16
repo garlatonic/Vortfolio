81f4409eba41d823e40cba783f974e886d60d85e
ivoryShield <sangapark34@naver.com>
2026-04-16 22:24:39 +0900

    feat: 마우스 포인터 컴포넌트 및 훅 추가

---
+++ src/hooks/useMousePosition.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export function useMousePosition() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [cursor, setCursor] = useState("default");
+
+  const handleMouseMove = (event: MouseEvent) => {
+    setPosition({ x: event.clientX, y: event.clientY });
+  };
+
+  const handleMouseOver = (event: MouseEvent) => {
+    const target = event.target as HTMLElement;
+    if (target.tagName === "A" || target.closest("a")) {
+      setCursor("pointer");
+    } else {
+      setCursor("default");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseover", handleMouseOver);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseover", handleMouseOver);
+    };
+  }, []);
+
+  return {
+    x: position.x,
+    y: position.y,
+    cursor,
+  };
+}
