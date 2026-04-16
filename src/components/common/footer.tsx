de54eca7e79cba001d7c4f0ee304b670f1eb1c84
ivoryShield <sangapark34@naver.com>
2026-04-16 22:27:56 +0900

    feat: 푸터 컴포넌트에 Magnet 컴포넌트 추가 및 링크 업데이트

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,27 +1,58 @@
 import { ArrowUpRightIcon } from "lucide-react";
 import Link from "next/link";
+import { Separator } from "../ui/separator";
+import Magnet from "../react-bits/magnet";
 
 export default function Footer() {
   return (
     <footer className="footer bg-primary">
-      <div className="inner flex py-20 justify-between">
-        <h2 className="uppercase font-black text-8xl text-accent -tracking-widest leading-none flex flex-col xl:flex-row gap-x-6">
+      <div className="inner flex flex-col lg:flex-row py-15 md:py-20 justify-between gap-15">
+        <h2 className="uppercase font-black text-6xl md:text-8xl text-accent leading-none flex flex-wrap gap-x-6">
           <span>get in</span>
           <span className="text-transparent text-outline ">touch</span>
         </h2>
-        <div className="flex flex-col gap-6 mt-16 lg:mt-0 text-accent  font-medium text-4xl [&&>svg]:size-6 xl:[&&>svg]:size-6">
-          <Link href="" target="_blank" className="flex gap-1">
-            Velog
-            <ArrowUpRightIcon />
-          </Link>
-          <Link href="" target="_blank" className="flex gap-1">
-            Github
-            <ArrowUpRightIcon />
-          </Link>
-          <Link href="" target="_blank" className="flex gap-1">
-            Linkedin
-            <ArrowUpRightIcon />
-          </Link>
+        <Separator className="lg:hidden" />
+        <div className="flex flex-col gap-6 lg:mt-0 text-accent font-medium text-2xl md:text-4xl [&&>svg]:size-6 xl:[&&>svg]:size-6 lg:flex-none flex-1">
+          <Magnet padding={10} magnetStrength={10}>
+            <Link
+              href="https://velog.io/@garlatonic"
+              target="_blank"
+              className="inline-flex gap-1"
+            >
+              Velog
+              <ArrowUpRightIcon />
+            </Link>
+          </Magnet>
+          <Magnet padding={10} magnetStrength={10}>
+            <Link
+              href="https://github.com/garlatonic"
+              target="_blank"
+              className="inline-flex gap-1"
+            >
+              Github
+              <ArrowUpRightIcon />
+            </Link>
+          </Magnet>
+          <Magnet padding={10} magnetStrength={10}>
+            <Link
+              href="https://www.linkedin.com/in/sanga-park-495630387/"
+              target="_blank"
+              className="inline-flex gap-1"
+            >
+              Linkedin
+              <ArrowUpRightIcon />
+            </Link>
+          </Magnet>
+          <Magnet padding={10} magnetStrength={10}>
+            <Link
+              href="mailto:garlatonic@kakao.com"
+              target="_blank"
+              className="inline-flex gap-1"
+            >
+              MailTo
+              <ArrowUpRightIcon />
+            </Link>
+          </Magnet>
         </div>
       </div>
     </footer>
